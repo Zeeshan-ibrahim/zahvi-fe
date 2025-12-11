1fc1cd299d655e0e9e4eb5b93fefc81978f2feb8
Zeeshan <zeeshanibrahim78@gmail.com>
2025-12-11 21:08:34 +0500

    Login setup

--- dal/auth/auth.services.ts
+++ dal/auth/auth.services.ts
@@ -1,27 +1,23 @@
 import axios from "axios";
-import { useMutation,useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { LoginData, SignupData } from "@/dal/auth/auth.types";
+import { env } from "process";
 
 export const useLogin = () => {
-    const queryClient = useQueryClient();
     return useMutation({
         mutationFn: async (data: LoginData) => {
-            const response = await axios.post("/api/login", data);
+            const response = await axios.post(`${env.BACKEND_URL}/api/login`, data);
             return response.data;
         },
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ["user"] });
-          },
     });
 };
 
 export const useSignup = () => {
-    const queryClient = useQueryClient();
+    // const queryClient = useQueryClient();
     return useMutation({
         mutationFn: async (data: SignupData) => {
             const response = await axios.post("/api/signup", data);
             return response.data;
         },
     });
-    queryClient.invalidateQueries({ queryKey: ['todos'] })
 };
